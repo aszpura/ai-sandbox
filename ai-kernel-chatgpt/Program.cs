79d98c958977707b348c2a54eaf1d1b7c1e2cc95
andrzej.szpura <andrzej.szpura@hotmail.com>
2025-05-02 17:36:13 +0100

    Add first base app using semantic kernel

---
+++ ai-kernel-chatgpt/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.Extensions.Configuration;
+
+
+IConfigurationRoot config = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+
+var builder = Kernel.CreateBuilder();
+
+
+builder.AddOpenAIChatCompletion(
+    modelId: "gpt-3.5-turbo", // or "gpt-4"
+    apiKey: config["API_KEY"] ?? throw new InvalidOperationException("API_KEY is not set in the environment variables.")
+);
+
+
+var kernel = builder.Build();
+
+// Get the chat completion service
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Create chat history (you can add system prompts, user messages, etc.)
+ChatHistory chat = [];
+
+chat.AddSystemMessage("You are a helpful assistant.");
+chat.AddUserMessage("What is captial city of Canada?");
+
+// Get response
+var reply = await chatService.GetChatMessageContentAsync(chat);
+Console.WriteLine(reply?.Content);
+
